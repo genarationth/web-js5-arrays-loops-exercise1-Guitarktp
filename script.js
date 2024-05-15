bcb31aac562419501b95dc0793ec7ba17321625d
Guitar <kittipong.sataya@gmail.com>
2024-05-15 13:38:54 +0700

    first summit

---
+++ script.js
@@ -0,0 +1,23 @@
+//Exercise 1.1
+//- What is the length of the array?
+// Ans 7
+//- Write a function called myAlphabetLength which console.logs the length of the array.
+// let myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+// console.log(myAlphabet.length)
+
+//Exercise 1.2
+// let planets = ['pluto', 'earth', 'saturn', 'neptune', 'jupiter'];
+// console.log(planets);
+
+// planets.forEach(function(eachPlanet) {
+//     console.log(eachPlanet);
+// });
+
+//Exercise 1.3
+let myArr = [ 1, 2, 'One', true];
+myArr.forEach(function(type) {
+    console.log(type);
+});
+
+
+
